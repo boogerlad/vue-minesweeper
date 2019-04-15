f1361f7b1e599b2a0ae9b8c1484dfaf423fc6a93
kevin <boogerlad@gmail.com>
2019-04-15 01:42:59 -0400

    comments

--- minesweeper.js
+++ minesweeper.js
@@ -13,18 +13,22 @@ class Grid {
 		this.tiles = new Array(vertical * horizontal);
 		this.bombs = Math.min(bombs, this.tiles.length);
 		this.hidden = this.tiles.length;
+		//populate the beginning of the array with bombs
 		for(let i = 0; i < this.bombs; ++i) {
 			this.tiles[i] = new Tile(9);
 		}
+		//populate the rest of the array with non-bombs. number of bombs adjacent to each tile cannot be known until after shuffling
 		for(let i = this.bombs; i < this.tiles.length; ++i) {
 			this.tiles[i] = new Tile(0);
 		}
+		//https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
 		for(let i = this.tiles.length - 1; i > 0; --i) {
 			let temp = this.tiles[i];
 			let randomIndex = Math.trunc(Math.random() * (i + 1));
 			this.tiles[i] = this.tiles[randomIndex];
 			this.tiles[randomIndex] = temp;
 		}
+		//for each bomb, increment adjacent tiles' bomb counts by one
 		for(let i = 0; i < this.tiles.length; ++i) {
 			if(this.tiles[i].bombCount === 9) {
 				this.adjacent(...this.one2two(i), index => {
@@ -35,12 +39,15 @@ class Grid {
 			}
 		}
 	}
+	//since grid is represented by a 1 dimensional array instead of a 2 dimensional one, need to do some math to get the row #, column #
 	one2two(index) {
 		return [Math.trunc(index / this.horizontal), index % this.horizontal];
 	}
+	//and vice versa
 	two2one(row, column) {
 		return row * this.horizontal + column;
 	}
+	//can pass in a function to apply function to all tiles adjacent to row, column
 	adjacent(row, column, f) {
 		let maxRow = Math.min(this.vertical, row + 2);
 		let maxColumn = Math.min(this.horizontal, column + 2);
@@ -55,6 +62,7 @@ class Grid {
 	reveal(row, column) {
 		this.actuallyReveal(this.two2one(row, column));
 	}
+	//recursively reveal when no bombs for convenience
 	actuallyReveal(index) {
 		let tile = this.tiles[index];
 		if(!tile.revealed && !tile.flagged) {
@@ -69,9 +77,9 @@ class Grid {
 		}
 	}
 	flag(row, column) {
-		let index = this.two2one(row, column);
-		if(!this.tiles[index].revealed) {
-			this.tiles[index].flagged = !this.tiles[index].flagged;
+		let tile = this.tiles[this.two2one(row, column)];
+		if(!tile.revealed) {
+			tile.flagged = !tile.flagged;
 		}
 	}
 }
